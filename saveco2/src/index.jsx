2f00f0a730fc0daefc05753addb0b5a1cc475d07
Andrea Příhoda <andyprihoda@gmail.com>
2021-05-23 22:24:15 +0200

    Eslint

--- saveco2/src/index.jsx
+++ saveco2/src/index.jsx
@@ -2,6 +2,5 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-const App = () => <div className="container"></div>;
-
+const App = () => <div className="container" />;
 render(<App />, document.querySelector('#app'));
