e9ed0b33f66ceddea010e418474e5ee0d1f46452
Andrea Příhoda <andyprihoda@gmail.com>
2021-05-24 20:13:43 +0200

    Fonts + icons

--- saveco2/src/index.jsx
+++ saveco2/src/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import MaterialIcon from 'react-google-material-icons';
 import './style.css';
 
-const App = () => <div className="container" />;
+// icon: Name of the Google Material icon: https://fonts.google.com/icons
+// Note that size is an optional parameter which is use to set the font size of the icon. Default size is 24px.
+
+const App = () => (
+  <div className="container">
+    <MaterialIcon icon="account_circle" size={48} />
+  </div>
+);
 render(<App />, document.querySelector('#app'));
