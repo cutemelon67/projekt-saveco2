e160e3e09f64c3c6a5aaecf38348a05bacf82f7a
“Markéta <melounova.marketa@gmail.com>
2021-05-25 23:08:34 +0200

    CSS starting-page

--- saveco2/src/components/Header/Header.jsx
+++ saveco2/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import './style.css';
 export const Header = (props) => {
   return (
     <header>
-      <img className="header__logo" src="" alt="saveCO2" />
+      <img className="header__logo" src="img/favicon.png" alt="saveCO2" />
       {/* <nav className="header__nav--language">
         <ul>
           <li>
