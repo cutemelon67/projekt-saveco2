1378fbf490e43e29dd06aa89991f132498a9c678
“Markéta <melounova.marketa@gmail.com>
2021-06-05 16:21:26 +0200

    CSS Methodology page

--- saveco2/src/components/MethodologyPage/MethodologyPage.jsx
+++ saveco2/src/components/MethodologyPage/MethodologyPage.jsx
@@ -26,20 +26,23 @@ export const MethodologyPage = ({ userData, setUserData }) => {
           Koeficienty použité pro výpočet emisí CO<sub>2</sub>
         </h2>
         <Nav href={'/results'}>Výsledky</Nav>
-        <div className="methodology__container">
-          <Methodology />
-        </div>
+        <div className="methodology__overview">
+          <div className="methodology__container">
+            <Methodology />
+          </div>
 
-        <div className="calculation__container">
-          <Calculation />
-        </div>
-        <div className="sources__container">
-          <Sources />
-        </div>
-        <div className="methodology__button">
-          <Button onClick={onSubmit}>
-            Zadej novou trasu <MaterialIcon icon={'chevron_right'} size={12} />
-          </Button>
+          <div className="calculation__container">
+            <Calculation />
+          </div>
+          <div className="sources__container">
+            <Sources />
+          </div>
+          <div className="methodology__button">
+            <Button onClick={onSubmit}>
+              Zadej novou trasu{' '}
+              <MaterialIcon icon={'chevron_right'} size={12} />
+            </Button>
+          </div>
         </div>
       </div>
       <Footer></Footer>
--- saveco2/src/components/MethodologyResults/MethodologyResults.jsx
+++ saveco2/src/components/MethodologyResults/MethodologyResults.jsx
@@ -7,20 +7,20 @@ import './methodologyResults.css';
 export const MethodologyResults = () => {
   return (
     <>
-      <div className="results__methodology--info">
+      <div className="results__methodology-header">
         <MaterialIcon icon={'info'} size={48} />
-        <div className="results__methodology--info-text">
-          <h3>Jak jsme na to přišli?</h3>
-          <p>
-            Počet kilometrů jsme vynásobili příslušným koeficientem podle
-            zvoleného dopravního prostředku a v&nbsp;případě auta také typu
-            pohonu. U&nbsp;pravidelných cest jsme toto číslo ještě vynásobili
-            počtem cest za jeden rok. Pokud vás cestuje více, u&nbsp;auta a
-            motorky jsme množství CO
-            <sub>2</sub> převedli na jednu osobu, aby bylo možné jednotlivé
-            dopravní prostředky mezi sebou porovnávat.
-          </p>
-        </div>
+        <h3>Jak jsme na to přišli?</h3>
+      </div>
+      <div className="results__methodology-text">
+        <p>
+          Počet kilometrů jsme vynásobili příslušným koeficientem podle
+          zvoleného dopravního prostředku a v&nbsp;případě auta také typu
+          pohonu. U&nbsp;pravidelných cest jsme toto číslo ještě vynásobili
+          počtem cest za jeden rok. Pokud vás cestuje více, u&nbsp;auta a
+          motorky jsme množství CO
+          <sub>2</sub> převedli na jednu osobu, aby bylo možné jednotlivé
+          dopravní prostředky mezi sebou porovnávat.
+        </p>
       </div>
       <Link to="/methodology">
         <Button className={'btn--center'}>Více k metodice</Button>
