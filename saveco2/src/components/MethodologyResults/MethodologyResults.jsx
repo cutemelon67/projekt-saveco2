15579668bdf1fe4ed297f838d4960c1fc6499df1
“Markéta <melounova.marketa@gmail.com>
2021-06-05 14:17:54 +0200

    CSS Jak jsme na to přišli

--- saveco2/src/components/MethodologyResults/MethodologyResults.jsx
+++ saveco2/src/components/MethodologyResults/MethodologyResults.jsx
@@ -7,18 +7,20 @@ import './methodologyResults.css';
 export const MethodologyResults = () => {
   return (
     <>
-      <MaterialIcon icon={'info'} size={48} />
       <div className="results__methodology--info">
-        <h3>Jak jsme na to přišli?</h3>
-        <p>
-          Počet kilometrů jsme vynásobili příslušným koeficientem podle
-          zvoleného dopravního prostředku a v&nbsp;případě auta také typu
-          pohonu. U&nbsp;pravidelných cest jsme toto číslo ještě vynásobili
-          počtem cest za jeden rok. Pokud vás cestuje více, u&nbsp;auta a
-          motorky jsme množství CO
-          <sub>2</sub> převedli na jednu osobu, aby bylo možné jednotlivé
-          dopravní prostředky mezi sebou porovnávat.
-        </p>
+        <MaterialIcon icon={'info'} size={48} />
+        <div className="results__methodology--info-text">
+          <h3>Jak jsme na to přišli?</h3>
+          <p>
+            Počet kilometrů jsme vynásobili příslušným koeficientem podle
+            zvoleného dopravního prostředku a v&nbsp;případě auta také typu
+            pohonu. U&nbsp;pravidelných cest jsme toto číslo ještě vynásobili
+            počtem cest za jeden rok. Pokud vás cestuje více, u&nbsp;auta a
+            motorky jsme množství CO
+            <sub>2</sub> převedli na jednu osobu, aby bylo možné jednotlivé
+            dopravní prostředky mezi sebou porovnávat.
+          </p>
+        </div>
       </div>
       <Link to="/methodology">
         <Button className={'btn--center'}>Více k metodice</Button>
